8412e4cf85a3e48843dc305f4d91ef05e48040ae
Karine66 <shon@live.fr>
2021-02-20 18:25:35 +0100

    constraintLayout + sharedPref

--- app/src/main/java/com/karine/blocnote/MainActivity.kt
+++ app/src/main/java/com/karine/blocnote/MainActivity.kt
@@ -1,29 +1,42 @@
 package com.karine.blocnote
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.karine.blocnote.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    var fullText = ""
+    val SETTINGS_FILE_NAME = "com.karine.blocnote"
+    val FULLTEXT_SETTINGS_KEY = "fullText"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mainBinding.root
         setContentView(view)
+        val preferencesReader = getSharedPreferences( SETTINGS_FILE_NAME, MODE_PRIVATE)
+        fullText = preferencesReader.getString(FULLTEXT_SETTINGS_KEY, "").toString()
+        mainBinding.fullTextView.text = fullText
     }
 
     @SuppressLint("SetTextI18n")
     fun addButtonTouched(button: View) {
         val userInput = mainBinding.userField.text
-        val existingText =  mainBinding.fullTextView.text
-         mainBinding.fullTextView.text ="$existingText\n$userInput"
+        fullText = "$fullText\n$userInput"
+        saveTextToSharedPref()
+        mainBinding.fullTextView.text = fullText
         mainBinding.userField.text = null
+    }
+
+    fun saveTextToSharedPref() {
+        val sharedPreferences = getSharedPreferences(SETTINGS_FILE_NAME, MODE_PRIVATE).edit()
+        sharedPreferences.putString(FULLTEXT_SETTINGS_KEY, fullText)
+        sharedPreferences.apply()
     }
 }
 
